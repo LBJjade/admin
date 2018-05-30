df0f3b649001fe359c3ad95a347a9d54814a79bb
lbj <2105931798@qq.com>
2018-05-30 12:13:28 +0800

    用户验证页面

--- src/routes/System/UserAuth.js
+++ src/routes/System/UserAuth.js
@@ -81,52 +81,51 @@ export default class BasicForms extends PureComponent {
     };
 
     return (
-      <PageHeaderLayout content="">
-        <Card title="实名认证">
-          <Row gutter={16}>
-            <Col span={12} gutter={16}>
-              <Card title="账号信息">
-                <Form
-                  hideRequiredMark
-                  style={{ marginTop: 8 }}
+      <PageHeaderLayout title="实名认证" content="">
+        <Row gutter={16}>
+          <Col span={12} gutter={16}>
+            <Card title="账号信息">
+              <Form
+                hideRequiredMark
+                style={{ marginTop: 8 }}
+              >
+                <FormItem
+                  {...formItemLayout}
+                  label="昵称"
                 >
-                  <FormItem
-                    {...formItemLayout}
-                    label="昵称"
-                  >
-                    {(
-                      <Tooltip title="prompt text">
-                        <span>{user.username}</span>
-                      </Tooltip>
+                  {(
+                    <Tooltip title="prompt text">
+                      <span>{user.username}</span>
+                    </Tooltip>
                         )}
-                  </FormItem>
-                  <FormItem
-                    {...formItemLayout}
-                    label="会员类型"
-                  >
-                    {(
-                      <Tag color="red">未认证</Tag>
+                </FormItem>
+                <FormItem
+                  {...formItemLayout}
+                  label="会员类型"
+                >
+                  {(
+                    <Tag color="red">未认证</Tag>
                         )}
-                  </FormItem>
-                  <FormItem
-                    {...formItemLayout}
-                    label="注册时间"
-                  >
-                    {(
-                      <Tooltip title="prompt text">
-                        <span>{moment(user.createdAt).format('YYYY-MM-DD hh:mm')}</span>
-                      </Tooltip>
+                </FormItem>
+                <FormItem
+                  {...formItemLayout}
+                  label="注册时间"
+                >
+                  {(
+                    <Tooltip title="prompt text">
+                      <span>{moment(user.createdAt).format('YYYY-MM-DD hh:mm')}</span>
+                    </Tooltip>
                         )}
-                  </FormItem>
-                </Form>
-              </Card>
-              <div style={{ height: 16 }} />
-              <Card title="收货地址">
-                {address.results === undefined || address.results.length === 0 ? (<div style={{ textAlign: 'center' }}>暂无数据</div>) : (
-                  <div>
-                    <List
-                      dataSource={address.results}
-                      renderItem={item => (
+                </FormItem>
+              </Form>
+            </Card>
+            <div style={{ height: 16 }} />
+            <Card title="收货地址">
+              {address.results === undefined || address.results.length === 0 ? (<div style={{ textAlign: 'center' }}>暂无数据</div>) : (
+                <div>
+                  <List
+                    dataSource={address.results}
+                    renderItem={item => (
                             (
                               <div>
                                 {item.isDefault === true ? (
@@ -167,10 +166,10 @@ export default class BasicForms extends PureComponent {
                                 ) : ''}
                               </div>
                             ))}
-                    />
-                    <List
-                      dataSource={address.results}
-                      renderItem={item => (
+                  />
+                  <List
+                    dataSource={address.results}
+                    renderItem={item => (
                             (
                               <div>
                                 {item.isDefault === false ? (
@@ -211,21 +210,21 @@ export default class BasicForms extends PureComponent {
                                 ) : ''}
                               </div>
                             ))}
-                    />
-                  </div>
+                  />
+                </div>
                     )}
-              </Card>
-            </Col>
-            <Col span={12}>
-              <Card title="实名认证">
-                <Form
-                  style={{ marginTop: 8 }}
+            </Card>
+          </Col>
+          <Col span={12}>
+            <Card title="实名认证">
+              <Form
+                style={{ marginTop: 8 }}
+              >
+                <FormItem
+                  {...formItemLayout}
+                  label="认证姓名"
                 >
-                  <FormItem
-                    {...formItemLayout}
-                    label="认证姓名"
-                  >
-                    {getFieldDecorator('authName', {
+                  {getFieldDecorator('authName', {
                             rules: [{
                               required: true, message: '姓名不能为空',
                             }],
@@ -237,12 +236,12 @@ export default class BasicForms extends PureComponent {
                               prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
                             />
                           )}
-                  </FormItem>
-                  <FormItem
-                    {...formItemLayout}
-                    label="身份证号"
-                  >
-                    {getFieldDecorator('indentityCard', {
+                </FormItem>
+                <FormItem
+                  {...formItemLayout}
+                  label="身份证号"
+                >
+                  {getFieldDecorator('indentityCard', {
                             rules: [
                               { required: true, message: '身份证号不能为空' },
                               { fieldname: 'indentityCard', required: true, pattern: /^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/, message: '身份证号格式错误！' },
@@ -255,21 +254,21 @@ export default class BasicForms extends PureComponent {
                               prefix={<Icon type="idcard" style={{ color: 'rgba(0,0,0,.25)' }} />}
                             />
                           )}
-                  </FormItem>
-                  <FormItem
-                    {...formItemLayout}
-                    label="身份证"
-                  >
-                    {getFieldDecorator('goal', {
+                </FormItem>
+                <FormItem
+                  {...formItemLayout}
+                  label="身份证"
+                >
+                  {getFieldDecorator('goal', {
                           })(
                             <TextArea style={{ minHeight: 32 }} placeholder="" rows={4} />
                           )}
-                  </FormItem>
-                  <FormItem
-                    {...formItemLayout}
-                    label="联系手机"
-                  >
-                    {getFieldDecorator('mobile', {
+                </FormItem>
+                <FormItem
+                  {...formItemLayout}
+                  label="联系手机"
+                >
+                  {getFieldDecorator('mobile', {
                             rules: [
                               { fieldname: 'mobile', required: true, message: '联系手机不能为空' },
                               { fieldname: 'mobile', required: true, pattern: /^1\d{10}$/, message: '手机号码格式错误！' },
@@ -282,83 +281,82 @@ export default class BasicForms extends PureComponent {
                               prefix={<Icon type="mobile" style={{ color: 'rgba(0,0,0,.25)' }} />}
                             />
                           )}
-                  </FormItem>
-                  <FormItem
-                    {...formItemLayout}
-                    label="联系电话"
-                  >
-                    {getFieldDecorator('tel', {
+                </FormItem>
+                <FormItem
+                  {...formItemLayout}
+                  label="联系电话"
+                >
+                  {getFieldDecorator('tel', {
                           })(
                             <Input
                               placeholder="请输入联系电话"
                               prefix={<Icon type="phone" style={{ color: 'rgba(0,0,0,.25)' }} />}
                             />
                           )}
-                  </FormItem>
-                  <FormItem
-                    {...formItemLayout}
-                    label="E-mail"
-                  >
-                    {getFieldDecorator('email', {
+                </FormItem>
+                <FormItem
+                  {...formItemLayout}
+                  label="E-mail"
+                >
+                  {getFieldDecorator('email', {
                           })(
                             <Input
                               placeholder="请输入E-mail"
                               prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
                             />
                           )}
-                  </FormItem>
-                  <FormItem
-                    {...formItemLayout}
-                    label="QQ"
-                  >
-                    {getFieldDecorator('QQ', {
+                </FormItem>
+                <FormItem
+                  {...formItemLayout}
+                  label="QQ"
+                >
+                  {getFieldDecorator('QQ', {
                           })(
                             <Input
                               placeholder="请输入QQ"
                               prefix={<Icon type="qq" style={{ color: 'rgba(0,0,0,.25)' }} />}
                             />
                           )}
-                  </FormItem>
-                  <FormItem
-                    {...formItemLayout}
-                    label="微信"
-                  >
-                    {getFieldDecorator('wechat', {
+                </FormItem>
+                <FormItem
+                  {...formItemLayout}
+                  label="微信"
+                >
+                  {getFieldDecorator('wechat', {
                           })(
                             <Input
                               placeholder="请输入微信"
                               prefix={<Icon type="wechat" style={{ color: 'rgba(0,0,0,.25)' }} />}
                             />
                           )}
-                  </FormItem>
-                  <FormItem
-                    {...formItemLayout}
-                    label="标签"
-                  >
-                    {getFieldDecorator('tags', {
+                </FormItem>
+                <FormItem
+                  {...formItemLayout}
+                  label="标签"
+                >
+                  {getFieldDecorator('tags', {
                           })(
                             <Select mode="tags" placeholder="请输入标签" prefix={<Icon type="tags" style={{ color: 'rgba(0,0,0,.25)' }} />} />
                           )}
-                  </FormItem>
-                  <FormItem
-                    {...formItemLayout}
-                    label="备注"
-                  >
-                    {getFieldDecorator('remark')(
-                      <TextArea style={{ minHeight: 32 }} placeholder="请输入备注" rows={4} prefix={<Icon type="tags" style={{ color: 'rgba(0,0,0,.25)' }} />} />
+                </FormItem>
+                <FormItem
+                  {...formItemLayout}
+                  label="备注"
+                >
+                  {getFieldDecorator('remark')(
+                    <TextArea style={{ minHeight: 32 }} placeholder="请输入备注" rows={4} prefix={<Icon type="tags" style={{ color: 'rgba(0,0,0,.25)' }} />} />
                           )}
-                  </FormItem>
-                  <FormItem {...submitFormLayout} style={{ marginTop: 32 }}>
-                    <Button type="primary" style={{ marginRight: 8 }}><Link to="/system/user">返回</Link></Button>
-                    <Button type="primary" htmlType="button" onClick={e => this.handleSubmit(e)}>
+                </FormItem>
+                <FormItem {...submitFormLayout} style={{ marginTop: 32 }}>
+                  <Button type="primary" style={{ marginRight: 8 }}><Link to="/system/user">返回</Link></Button>
+                  <Button type="primary" htmlType="button" onClick={e => this.handleSubmit(e)}>
                             提交
-                    </Button>
-                  </FormItem>
-                </Form>
-              </Card>
-            </Col>
-          </Row>
-        </Card>
+                  </Button>
+                </FormItem>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
       </PageHeaderLayout>
     );
   }
